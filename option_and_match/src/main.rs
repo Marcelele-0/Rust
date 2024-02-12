0ffd6597fbe685011ca6b2f861e6ccbfe609bd0e
Marcelele-0 <mnem12321@gmail.com>
2024-02-12 23:22:05 +0100

    finished lesson

--- option_and_match/src/main.rs
+++ option_and_match/src/main.rs
@@ -24,54 +24,69 @@
 
 // }
 
+// fn main() {
+//     value_in_cents(Coin::Quarter(UsState::Alaska));
+
+//     let five = Some(5);
+//     let six = plus_one(five);
+//     let none = plus_one(None);
+
+//     println!("{:?}", six);  
+//     println!("{:?}", none);
+
+// }
+
+// #[derive(Debug)]
+// enum UsState {
+//     Alabama,
+//     Alaska,
+//     // --snip--
+// }
+
+// enum Coin {
+//     Penny,
+//     Nickel,
+//     Dime,
+//     Quarter(UsState),
+// }
+
+// fn value_in_cents(coin: Coin) -> u8 {
+//     match coin {
+//         Coin::Penny => {
+//             println!("Lucky Penny!");
+//             1
+//         }
+//         Coin::Nickel => 5,
+//         Coin::Dime => 10,
+//         Coin::Quarter(state) => {
+//             println!("State quarter from {:?}!", state);
+//             25
+//         }
+//     }
+// }
+
+
+// fn plus_one(x: Option<i32>) -> Option<i32> { 
+//     match x {
+//         Some(i) => Some(i + 1),
+//         _ => None, // placeholder for all other cases
+//     }
+// }
+
 fn main() {
-    value_in_cents(Coin::Quarter(UsState::Alaska));
+    let some_value = Some(3);
+    match some_value {
+        Some(3) => println!("three"),
+        _ => (), // otherwise do nothing
+    } // here we specify all the possible values that some_value could have and what we want to do in each case
 
-    let five = Some(5);
-    let six = plus_one(five);
-    let none = plus_one(None);
+    if let Some(3) = some_value { // if let, some value = some 3 => print three
+        println!("three"); 
+    } // here we specify a pattern and a value, and the code runs if the value matches the pattern
 
-    println!("{:?}", six);  
-    println!("{:?}", none);
-
+    
 }
 
-#[derive(Debug)]
-enum UsState {
-    Alabama,
-    Alaska,
-    // --snip--
-}
-
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter(UsState),
-}
-
-fn value_in_cents(coin: Coin) -> u8 {
-    match coin {
-        Coin::Penny => {
-            println!("Lucky Penny!");
-            1
-        }
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(state) => {
-            println!("State quarter from {:?}!", state);
-            25
-        }
-    }
-}
-
-
-fn plus_one(x: Option<i32>) -> Option<i32> { 
-    match x {
-        Some(i) => Some(i + 1),
-        _ => None, // placeholder for all other cases
-    }
-}
 
 
 
