d0d2445a09fe20607879cc7def96c530c20ed360
Marcelele-0 <mnem12321@gmail.com>
2024-03-01 22:40:43 +0100

    Added Vec<(x,x)> to handle n events

--- text_game/src/events.rs
+++ text_game/src/events.rs
@@ -1,71 +1,32 @@
 // events.rs
 
-use crate::Creature;
+use crate::creatures::Creature;
 
 pub struct TravelingDualChoice {
     pub location: String,
     pub description: String,
-    pub choice1: String,
-    pub choice2: String,
-    pub outcome1: String,
-    pub outcome2: String,
+    pub choices: Vec<(String, String)>,
 }
 
 impl TravelingDualChoice {
-    pub fn new(location: &str, description: &str, choice1: &str, choice2: &str, outcome1: &str, outcome2: &str) -> Self {
+    pub fn new(location: &str, description: &str, choices: &[(String, String)]) -> Self {
         TravelingDualChoice {
             location: location.to_string(),
             description: description.to_string(),
-            choice1: choice1.to_string(),
-            choice2: choice2.to_string(),
-            outcome1: outcome1.to_string(),
-            outcome2: outcome2.to_string(),
+            choices: choices.to_vec(),
         }
     }
 
     pub fn display(&self) {
-        println!("\n{}", self.description);
-        println!("1: {}", self.choice1);
-        println!("2: {}", self.choice2);
-    }
-}
-
-pub struct TravelingTripleChoice {
-    pub location: String,
-    pub description: String,
-    pub choice1: String,
-    pub choice2: String,
-    pub choice3: String,
-    pub outcome1: String,
-    pub outcome2: String,
-    pub outcome3: String,
-}
-
-impl TravelingTripleChoice {
-    pub fn new(location: &str, description: &str, choice1: &str, choice2: &str, choice3: &str, outcome1: &str, outcome2: &str, outcome3: &str) -> Self {
-        TravelingTripleChoice {
-            location: location.to_string(),
-            description: description.to_string(),
-            choice1: choice1.to_string(),
-            choice2: choice2.to_string(),
-            choice3: choice3.to_string(),
-            outcome1: outcome1.to_string(),
-            outcome2: outcome2.to_string(),
-            outcome3: outcome3.to_string(),
+        println!("{}", self.description);
+        for (i, choice) in self.choices.iter().enumerate() {
+            println!("{}. {}", i + 1, choice.0); // Print choice number and description
         }
     }
-
-    pub fn display(&self) {
-        println!("\n{}", self.description);
-        println!("1: {}", self.choice1);
-        println!("2: {}", self.choice2);
-        println!("3: {}", self.choice3);
-    }
-    
 }
+
 
 pub enum GameEvent {
     TravelingDualEvent(TravelingDualChoice),
     CombatEvent(Creature),
-    TravelingTripleEvent(TravelingTripleChoice),
 }
--- text_game/src/main.rs
+++ text_game/src/main.rs
@@ -9,59 +9,27 @@ mod game_state;
 use crate::creatures::Creature;
 use crate::game_state::GameState;
 use crate::events::TravelingDualChoice;
-use crate::events::TravelingTripleChoice;
 use crate::events::GameEvent;
 
 fn main() {
     let mut game_events: HashMap<String, GameEvent> = HashMap::new();
-    let mut game_state = GameState::new("Start");
+    let mut game_state = GameState::new("Crossroad");
 
-    game_events.insert("Start".to_string(), GameEvent::TravelingDualEvent(TravelingDualChoice::new(
-        "Start",
+    game_events.insert("Crossroad".to_string(), GameEvent::TravelingDualEvent(TravelingDualChoice::new(
+        "Crossroad",
         "You are at a crossroad. Which path will you take?",
-        "Go to Castle",
-        "Go to Forest",
-        "Castle",
-        "Forest",
+        &[
+            ("Go to Castle".to_string(),"castle".to_string()),
+            ("Go to Forest".to_string(),"forest".to_string())
+        ]
     )));
 
-    game_events.insert("Castle".to_string(), GameEvent::TravelingDualEvent(TravelingDualChoice::new(
-        "Castle",
-        "You are at the castle. What will you do?",
-        "Enter the castle",
-        "Keep walking",
-        "Indoor",
-        "Court",
-    )));
-
-    game_events.insert("Court".to_string(), GameEvent::TravelingDualEvent(TravelingDualChoice::new(
-        "Court",
-        "You are in the court. What will you do?",
-        "Go to the blacksmith",
-        "Leave the castle",
-        "Blacksmith",
-        "Suspicious_bridge",
-    )));
-
-    game_events.insert("Suspicious_bridge".to_string(), GameEvent::CombatEvent(Creature::new(
-        "Phantom", 
+    game_events.insert("Crossroad".to_string(), GameEvent::CombatEvent(Creature::new(
+        "Wolf",
         50, 
-        15, 
-        3,
-        "Bridge_ruins",
-    )));
-
-    game_events.insert("Bridge_ruins".to_string(), GameEvent::TravelingTripleEvent(TravelingTripleChoice::new(
-        "South of the bridge ruins",
-        "You are at the south of the bridge ruins. What will you do?",
-        "go back",
-        "Go up the river",
-        "follow the path",
-        "Castle",
-        "River",
-        "south path",
-
-    )));
+        5, 
+        2, 
+        "River")));
 
     let mut player = Creature::new("Player", 100, 10, 5," ",);
 
@@ -72,45 +40,35 @@ fn main() {
                     traveling_choice.display();
 
                     println!("Enter your choice (number): ");
+                    
                     let mut choice = String::new();
                     std::io::stdin().read_line(&mut choice).expect("Failed to read line");
+                    
                     let choice: usize = choice.trim().parse().expect("Please enter a number");
 
                     match choice {
-                        1 => game_state.transition(&traveling_choice.outcome1),
-                        2 => game_state.transition(&traveling_choice.outcome2),
-                        3 => break,
-                        _ => println!("Invalid choice, please enter 1 or 2."),
+                        0 => break,
+                        _ => {
+                        if choice > 0 && choice <= traveling_choice.choices.len() as usize {
+                            let choice_index = choice - 1;
+                            game_state.transition(&traveling_choice.choices[choice_index].1);
+                        } else {
+                            println!("Invalid choice, please enter a number between 1 and {}.", traveling_choice.choices.len());
+                        }}
                     }
-                },
+                }
+                
                 GameEvent::CombatEvent(creature) => {
                     let mut enemy = creature.clone();
                     player.fight(&mut enemy);
 
                     game_state.transition(&enemy.post_location); 
-                },
-                GameEvent::TravelingTripleEvent(traveling_choice) => {
-                    traveling_choice.display();
-
-                    println!("Enter your choice (number): ");
-                    let mut choice = String::new();
-                    std::io::stdin().read_line(&mut choice).expect("Failed to read line");
-                    let choice: usize = choice.trim().parse().expect("Please enter a number");
-
-                    match choice {
-                        1 => game_state.transition(&traveling_choice.outcome1),
-                        2 => game_state.transition(&traveling_choice.outcome2),
-                        3 => game_state.transition(&traveling_choice.outcome3),
-                        4 => break,
-                        _ => println!("Invalid choice, please enter 1, 2, or 3."),
-                    }
-                },
+                }
             }
         } else {
             println!("You wander into unknown territory.");
             break;
         }
-
         println!("You are now at: {}", game_state.current_location);
     }
 }
